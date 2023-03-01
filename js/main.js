f9659a57554377b3e7b5423e393b269b2e41d18b
nishanka <nishanka.rupasinghe@gmail.com>
2023-03-02 05:02:01 +1000

    Modify main.js to improve performance

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-// Get the viewport height and multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener("load", setViewPortHeight);
+window.addEventListener("resize", setViewPortHeight);
+setViewPortHeight();
 
-// Listen to the resize event
-window.addEventListener('resize', () => {
-  // Execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+function setViewPortHeight() {
+    // Get the viewport height and multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 
 // Add fullpage view to sections
 var fullScreenPage = document.getElementById('fullpage');
